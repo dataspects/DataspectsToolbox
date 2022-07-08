3fd0c2271c98ee2f0986fe8420726ecf4e1aef11
Lex Sulzer <lex@dataspects.com>
2022-07-08 15:11:13 +0200

    a

--- Development/getDocument.sh
+++ Development/getDocument.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -s \
-  -X GET "$MEILI_SERVER/indexes/$INDEX/documents/dscan_144" \
+  -X GET "$MEILI_SERVER/indexes/$INDEX/documents/BtDimGtlwu4o7vuExbplOipv5q3TpHB63e-JBLitpJg" \
   -H "Authorization: Bearer $MEILI_MASTER_KEY" \
   | jq .
