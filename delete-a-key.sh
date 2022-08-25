7207b568dff8ead2b6c9c086977cf8d0fa3872d0
Lex Sulzer <lex@dataspects.com>
2022-08-25 11:11:16 +0200

    echo "Meili server at $MEILI_SERVER"

--- delete-a-key.sh
+++ delete-a-key.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Meili server at $MEILI_SERVER"
+read -p "Continue? (y/n)" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    exit
+fi
+
 curl -s \
   -X DELETE "$MEILI_SERVER/keys/cnpYLMeu7ef601a49254e5edc05a6ddc3dda8d3d90a907faf66be9329d1fa00133e52707" \
   -H "Authorization: Bearer $MEILI_MASTER_KEY" \
--- get-all-keys.sh
+++ get-all-keys.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Meili server at $MEILI_SERVER"
+read -p "Continue? (y/n)" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    exit
+fi
+
 curl -s \
   -X GET "$MEILI_SERVER/keys" \
   -H "Authorization: Bearer $MEILI_MASTER_KEY" \
